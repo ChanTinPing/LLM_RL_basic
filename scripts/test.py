5a6f658cd1c91bd79806acd91e9a45478635b184
root <tinpingchan@yahoo.com>
2025-08-20 11:23:52 +0800

    Stop tracking test.py

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-import verl, os
-
-print("verl 模块路径:", verl.__file__)
-print("verl 安装目录:", os.path.dirname(verl.__file__))
-
-'''
-(WorkerDict pid=35449) Flash Attention 2 only supports torch.float16 and torch.bfloat16 dtypes, but the current dype in Qwen3ForCausalLM is torch.float32. You should run training or inference using Automatic Mixed-Precision via the `with torch.autocast(device_type='torch_device'):` decorator, or load the model with the `torch_dtype` argument. Example: `model = AutoModel.from_pretrained("openai/whisper-tiny", attn_implementation="flash_attention_2", torch_dtype=torch.float16)`
-(WorkerDict pid=35449) Flash Attention 2 only supports torch.float16 and torch.bfloat16 dtypes, but the current dype in Qwen3Model is torch.float32. You should run training or inference using Automatic Mixed-Precision via the `with torch.autocast(device_type='torch_device'):` decorator, or load the model with the `torch_dtype` argument. Example: `model = AutoModel.from_pretrained("openai/whisper-tiny", attn_implementation="flash_attention_2", torch_dtype=torch.float16)`
-'''
